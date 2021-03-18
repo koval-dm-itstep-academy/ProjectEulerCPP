c6c37b6809887d0e894ce952387303d02584d98f
Danny15 <denys152004@ukr.net>
2021-03-18 17:01:30 +0200

    add 7,19 tasks

---
+++ Problem 19 (Counting Sundays)/Problem19.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+int solve() {
+    int month[12] = { 31,0,31,30,31,30,31,31,30,31,30,31 };
+    int day = 2;
+    int sunday = 0;
+    for (int i = 1901; i < 2001; i++) {
+        if (i % 4 == 0) {
+            month[1] = 29;
+        }
+        else {
+            month[1] = 28;
+        }
+        for (int j = 0; j < 12; j++) {
+            if (day % 7 == 0) {
+                sunday++;
+            }
+            day += month[j];
+        }
+    }
+    return sunday;
+}
+
+int main() {
+    std::cout << solve() << "\n";
+}
---
+++ Problem 7 (10001st prime)/Problem7.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+bool isPrime(int a)
+{
+    for (int i = 2; i <= a / 2; ++i)
+    {
+        if (a % i == 0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+int solve()
+{
+    int count = 2;
+    for (int i = 5; ; i = i + 2)
+    {
+        if (isPrime(i))
+        {
+            count++;
+        }
+        if (count == 10001)
+        {
+            return i;
+        }
+    }
+}
+
+int main() {
+    std::cout << solve() << "\n";
+}
