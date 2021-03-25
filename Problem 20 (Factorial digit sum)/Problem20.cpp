7e99c16c92cec428254007fbf727343614ae29c7
kitsa <kit.sakharov@gmail.com>
2021-03-25 16:25:54 +0200

    cosmetic changes

--- Problem 20 (Factorial digit sum)/Problem20.cpp
+++ Problem 20 (Factorial digit sum)/Problem20.cpp
@@ -1,23 +1,18 @@
-// Часть кода для нахождения больших факториалов взял тут https://www.geeksforgeeks.org/factorial-large-number/
-
 #include<iostream>
 using namespace std;
- 
-// Максимальное количество цифр на выходе
+
 #define MAX 500
  
 int multiply(int x, int res[], int res_size);
  
-int solve(int n) //Решение задачи
+int solve(int n)
 {
     int sum = 0;
     int res[MAX];
  
-    // Инициализация результата
     res[0] = 1;
     int res_size = 1;
  
-    // Применение простой факториальной формулы    
     for (int x=2; x<=n; x++)
         res_size = multiply(x, res, res_size);
  
@@ -27,9 +22,6 @@ int solve(int n) //Решение задачи
     return sum;
 }
  
-// Эта функция умножает x на число, представленное res []. res_size - это размер res [] или количество цифр в числе, представленном res []. 
-// Эта функция использует простую школьную математику для умножения. 
-// Эта функция может иметь значение res_size и возвращает новое значение res_size.
 int multiply(int x, int res[], int res_size)
 {
     int carry = 0;
@@ -52,7 +44,6 @@ int multiply(int x, int res[], int res_size)
     return res_size;
 }
  
-
 int main()
 {
     cout << solve(100) << "\n";
